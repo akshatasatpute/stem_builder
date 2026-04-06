a6c9d1b5fb27f20dc900b259808e20f42fa4daad
akshatasatpute <akshatasatpute1333@gmail.com>
2026-04-07 01:54:08 +0530

    fix: correct wildcard route for express v5

--- server.ts
+++ server.ts
@@ -162,7 +162,7 @@ async function startServer() {
 
     app.use(express.static(distPath));
 
-    app.get('*', (req, res) => {
+    app.get('/*', (req, res) => {
       res.sendFile(path.join(distPath, 'index.html'));
     });
   }
